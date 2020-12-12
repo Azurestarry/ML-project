db11e6605decefdf7fc8b4851a1241a5bd799d64
KevinD <kaiwendai@KevinDs-MacBook-Pro.local>
2020-12-12 23:13:58 +0800

    update demo

--- opencv_demo.py
+++ opencv_demo.py
@@ -8,8 +8,9 @@ import cv2
 import matplotlib as plt
 import numpy as np
 
-# load the model
+# load model
 model = torch.load('vgg.pth',map_location=torch.device('cpu'))
+
 
 
 face_clsfr=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -17,7 +18,7 @@ face_clsfr=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 source=cv2.VideoCapture(0)
 
 labels_dict={0:'unmasked',1:'masked',2:'incorrectly_masked'}
-color_dict={0:(255,0,0),1:(0,255,0),2:(0,0,255)}
+color_dict={0:(0,0,255),1:(0,255,0),2:(255,0,0)}
 
 while(True):
 
@@ -30,21 +31,18 @@ while(True):
 
     for x,y,w,h in faces:
         face_img=img[y:y+w,x:x+w]
-
-        # resize image and normalize it 
         resized=cv2.resize(face_img,(64,64))
         resized = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
         resized = resized.transpose((2,0,1))
         resized = torch.as_tensor(resized,dtype = torch.float32)
-        resized = (resized-0.5)/0.5
+        # normalize the image
+        resized = (resized/255-0.5)/0.5
         reshaped=resized.view(1,3,64,64)
-
-        # make prediction
+        # make predictions
         result=model(reshaped)
         pred = result.argmax(dim = 1,keepdim = True)
 
         label = int(pred[0][0])
-
         # draw the box with the predicted label
         cv2.rectangle(img,(x,y),(x+w,y+h),0,2)
         cv2.rectangle(img,(x,y-40),(x+w,y),color_dict[label],-1)
@@ -55,7 +53,7 @@ while(True):
         break
         
         
-    cv2.imshow('LIVE_DEMO',img)
+    cv2.imshow('LIVEDEMO',img)
     key=cv2.waitKey(1)
     
     if(key==27):
